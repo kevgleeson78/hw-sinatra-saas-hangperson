12a70a149e997648be60e2419193589f5ee8e060
Your NameKevin <kevgleeson78@gmail.com>
2019-01-31 16:44:27 +0000

    Winning conditions added

--- app.rb
+++ app.rb
@@ -45,12 +45,12 @@ class HangpersonApp < Sinatra::Base
   begin
     
     if(@game.guess(letter)==false)
-    flash[:message]= "You have already guessed that letter"
+    flash[:message]= "You have already used that letter."
     
     end
     # Capture the argument error for empty string or non alpha characters
     rescue ArgumentError
-      flash[:message] = "That is not a valid letter"
+      flash[:message] = "Invalid guess."
   end
   redirect '/show'
 end
@@ -61,16 +61,29 @@ end
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    if(@game.check_win_or_lose() == :win)
+     
+      redirect '/win'
+      
+    end
+    if(@game.check_win_or_lose()== :lose)
+      redirect '/lose'
+    
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+     flash[:message] = "You Win!"
+     
+   # erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    flash[:message] = "Sorry, you lose!"
+    
     erb :lose # You may change/remove this line
   end
   
